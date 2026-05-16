787555175cdd54f7764fd86cd55901e41554d4f5
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-05-16 19:26:23 +0200

    Separated game input from player class

---
+++ Assets/Scripts/GameInput.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class GameInput : MonoBehaviour {
+    public Vector2 GetMovementVectorNormalised() {
+        Vector2 inputVector = new Vector2(0, 0);
+
+        if (Input.GetKey(KeyCode.W)) {
+            inputVector.y += 1f;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            inputVector.y -= 1f;
+        }
+        if (Input.GetKey(KeyCode.A)) {
+            inputVector.x -= 1f;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            inputVector.x += 1f;
+        }
+        inputVector = inputVector.normalized;
+
+        return inputVector;
+    }
+}
