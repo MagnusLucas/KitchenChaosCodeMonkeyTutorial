690514e2188a2ae27318ff388d1e05ac500e9408
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-05-16 16:22:07 +0200

    Added idle and walking animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ public class Player : MonoBehaviour {
     [SerializeField] private float moveSpeed = 1f;
     [SerializeField] private float rotateSpeed = 0.1f;
 
+    private bool isWalking = false;
+
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
 
@@ -24,11 +26,16 @@ public class Player : MonoBehaviour {
 
         Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y) * Time.deltaTime * moveSpeed;
 
+        isWalking = moveDir != Vector3.zero;
+
         if (moveDir != Vector3.zero) {
             transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
-            //    moveDir;
             transform.position += moveDir;
         }
+    }
+
+    public bool IsWalking() {
+        return isWalking;
     }
 
 }
