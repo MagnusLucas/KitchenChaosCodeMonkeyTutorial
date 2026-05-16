cd0702be1a5a74c5efadbafd951a615a443a28cb
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-05-16 20:05:32 +0200

    Switched to the new (now default) input system

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -1,23 +1,19 @@
 using UnityEngine;
 
 public class GameInput : MonoBehaviour {
-    public Vector2 GetMovementVectorNormalised() {
-        Vector2 inputVector = new Vector2(0, 0);
 
-        if (Input.GetKey(KeyCode.W)) {
-            inputVector.y += 1f;
-        }
-        if (Input.GetKey(KeyCode.S)) {
-            inputVector.y -= 1f;
-        }
-        if (Input.GetKey(KeyCode.A)) {
-            inputVector.x -= 1f;
-        }
-        if (Input.GetKey(KeyCode.D)) {
-            inputVector.x += 1f;
-        }
+    private PlayerInputActions playerInputActions;
+    private void Awake() {
+        playerInputActions = new PlayerInputActions();
+        playerInputActions.Player.Enable();
+    }
+
+    public Vector2 GetMovementVectorNormalised() {
+        Vector2 inputVector = playerInputActions.Player.Move.ReadValue<Vector2>();
+
         inputVector = inputVector.normalized;
 
+        Debug.Log(inputVector);
         return inputVector;
     }
 }
