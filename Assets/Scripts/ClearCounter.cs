63ae3dd2ae31697220074d72b72a9ef7a28c169b
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-05-21 17:12:21 +0200

    Spawning tomatoes on counters

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -1,7 +1,12 @@
 using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
+
+    [SerializeField] private Transform tomatoPrefabTransform;
+    [SerializeField] private Transform spawnPoint;
+
     public void Interact() {
         Debug.Log("Interacted with clear counter");
+        Instantiate(tomatoPrefabTransform, spawnPoint);
     }
 }
