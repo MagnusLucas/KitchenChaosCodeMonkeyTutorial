c2b83e95572e8c24516945b2af93f80ff059a03d
MagnusLucas <lukasz.kisielewski@student.uj.edu.pl>
2026-05-21 20:34:06 +0200

    KitchenObject now knows where it's placed, counter knows what's on it

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -2,10 +2,39 @@ using UnityEngine;
 
 public class ClearCounter : MonoBehaviour {
 
-    [SerializeField] private KitchenObjectSO kitchenObject;
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
     [SerializeField] private Transform spawnPoint;
 
+    private KitchenObject kitchenObject;
+
     public void Interact() {
-        Instantiate(kitchenObject.prefab, spawnPoint);
+        if (kitchenObject == null) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, spawnPoint);
+            kitchenObject = kitchenObjectTransform.GetComponent<KitchenObject>();
+            kitchenObject.SetClearCounter(this);
+        } else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
     }
+
+    public Transform GetKitchenObjectSpawnPoint() {
+        return spawnPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject newKitchenObject) {
+        kitchenObject = newKitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject() {
+        return (kitchenObject != null);
+    }
+
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -4,6 +4,28 @@ public class KitchenObject : MonoBehaviour
 {
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
-    public KitchenObjectSO GetKitchenObjectSO() { return kitchenObjectSO; }
+    private ClearCounter clearCounter;
+
+    public KitchenObjectSO GetKitchenObjectSO() {
+        return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter newClearCounter) {
+        if (clearCounter != null) {
+            clearCounter.ClearKitchenObject();
+        }
+        clearCounter = newClearCounter;
+
+        if (clearCounter.HasKitchenObject()) {
+            Debug.LogError("Counter already has a set kitchen object!");
+        }
+
+        clearCounter.SetKitchenObject(this);
+        transform.parent = clearCounter.GetKitchenObjectSpawnPoint();
+    }
+
+    public ClearCounter GetClearCounter() {
+        return clearCounter;
+    }
 
 }
